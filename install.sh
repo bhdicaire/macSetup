6f0c62830efe9efb39481657242481c23300ac77
Benoît H. Dicaire <bh@dicaire.com>
2018-07-06 17:23:52 -0400

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+
+## Hombrew
+echo "Install Homebrew"
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+#export PATH=${HOMEBREW_DIR}/sbin:${HOMEBREW_DIR}/bin:${PATH}
+
+# Git
+# Echo "INstall Git"
+# brew install git
+# 
+# Git
+# Echo "Install Ansible and prerequisites"
+# brew install ansible
+# 
+# brew update
+# brew upgrade
+# brew list
